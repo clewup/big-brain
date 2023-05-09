f7f98295d7643830ef42243dc3cff260235679d3
clew <lewis@clewup.co.uk>
2023-05-09 22:56:40 +0100

    feat(tailwind): add tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <div className="min-h-screen w-screen bg-branding-beige">{children}</div>
+            </body>
         </html>
     );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: {
+        extend: {},
+        colors: {
+            branding: {
+                beige: '#f8fcda',
+                black: '#111111',
+            },
+        },
+    },
+    plugins: [],
+};

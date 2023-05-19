1376770430f6226f8b83462d9ec46166dc7826ef
clew <lewis@clewup.co.uk>
2023-05-20 00:46:28 +0100

    feat(create): extend form fields and functionality

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import PageWrapper from '@/components/PageWrapper/PageWrapper'
+import constants from '@/constants/constants'
 import { LockrContext } from '@/lib/lockr-auth/contexts/LockrContext'
-import { Field, Form, Formik, FormikHelpers, FormikValues } from 'formik'
-import { useContext } from 'react'
+import { Field, Form, Formik, FormikHelpers, FormikProps, FormikValues } from 'formik'
+import { useContext, useRef } from 'react'
+import { MultiValue } from 'react-select'
+import Select from 'react-select/creatable'
 
 export default function Create() {
+    const formRef = useRef<FormikProps<CreateFormValues>>(null)
     const { user } = useContext(LockrContext)
 
     type CreateFormValues = {
@@ -19,7 +23,7 @@ export default function Create() {
     const initialValues: CreateFormValues = {
         title: '',
         content: '',
-        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1336&q=80',
+        image: '',
         categories: [],
         comments: [],
     }
@@ -38,29 +42,83 @@ export default function Create() {
         formHelpers.setSubmitting(false)
     }
 
+    async function uploadImage(image: Blob | undefined) {
+        if (!image) return
+
+        const formData = new FormData()
+        formData.append('file', image)
+        formData.append('upload_preset', constants.CLOUDINARY_UPLOAD_PRESET)
+        formData.append('cloud_name', constants.CLOUDINARY_CLOUD_NAME)
+
+        const cloudinaryResponse = await fetch('https://api.cloudinary.com/v1_1/dliog6kq6/image/upload', {
+            method: 'POST',
+            body: formData,
+        })
+        const cloudinaryData = await cloudinaryResponse.json()
+
+        formRef?.current?.setFieldValue('image', cloudinaryData.url)
+    }
+
     return (
         <PageWrapper>
-            <Formik initialValues={initialValues} onSubmit={onSubmit}>
-                {() => {
+            <Formik initialValues={initialValues} onSubmit={onSubmit} innerRef={formRef}>
+                {({ values, setFieldValue }) => {
+                    console.log(values)
                     return (
                         <Form>
                             <span className="form-control">
                                 <label className="label">Title</label>
                                 <Field name="title" type="text" className="input input-bordered" />
                             </span>
+
                             <span className="form-control">
                                 <label className="label">Content</label>
-                                <Field name="content" type="text" className="input input-bordered" />
+                                <Field name="content" type="text">
+                                    {() => <textarea className="textarea textarea-bordered h-96"></textarea>}
+                                </Field>
+                            </span>
+
+                            {values.image && (
+                                <div className="mt-5 h-[300px] w-[300px] avatar">
+                                    <img src={values.image} alt="post_image" className="mask mask-squircle" />
+                                </div>
+                            )}
+
+                            <span className="form-control">
+                                <label className="label">Image</label>
+                                <input
+                                    type="file"
+                                    className="file-input file-input-bordered"
+                                    onChange={({ target: { files } }) => uploadImage(files?.[0])}
+                                />
                             </span>
 
                             <span className="form-control">
                                 <label className="label">Categories</label>
-                                <select className="select select-bordered">
-                                    <option>Select...</option>
-                                    <option>Technology</option>
-                                    <option>Gaming</option>
-                                    <option>News</option>
-                                </select>
+                                <Select
+                                    isMulti={true}
+                                    onChange={(options: MultiValue<{ label: string; value: string }>) => {
+                                        const flattenedOptions = options.flatMap((option) => option.value)
+                                        setFieldValue('categories', flattenedOptions)
+                                    }}
+                                    theme={(theme) => ({
+                                        ...theme,
+                                        borderRadius: 7,
+                                        colors: {
+                                            ...theme.colors,
+                                            primary: '#CCCCCC',
+                                        },
+                                    })}
+                                    classNames={{
+                                        control: () => 'h-12',
+                                    }}
+                                    styles={{
+                                        control: (provided, state) => ({
+                                            ...provided,
+                                            outline: '0 0 0 2px #CCCCCC',
+                                        }),
+                                    }}
+                                />
                             </span>
 
                             <div className="mt-10">
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,7 @@
 const constants = {
     APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME as string,
+    CLOUDINARY_UPLOAD_PRESET: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET as string,
 }
+
+export default constants

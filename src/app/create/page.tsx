0afa276eba02a513794cec9464ddc63d5e3964f0
clew <lewis@clewup.co.uk>
2023-05-20 01:04:07 +0100

    feat(create): add validation schema

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,10 +3,11 @@
 import PageWrapper from '@/components/PageWrapper/PageWrapper'
 import constants from '@/constants/constants'
 import { LockrContext } from '@/lib/lockr-auth/contexts/LockrContext'
-import { Field, Form, Formik, FormikHelpers, FormikProps, FormikValues } from 'formik'
+import { ErrorMessage, Field, Form, Formik, FormikHelpers, FormikProps, FormikValues } from 'formik'
 import { useContext, useRef } from 'react'
 import { MultiValue } from 'react-select'
 import Select from 'react-select/creatable'
+import * as yup from 'yup'
 
 export default function Create() {
     const formRef = useRef<FormikProps<CreateFormValues>>(null)
@@ -27,6 +28,26 @@ export default function Create() {
         categories: [],
         comments: [],
     }
+
+    const validationSchema = yup.object().shape({
+        title: yup
+            .string()
+            .typeError('Title must be a string')
+            .min(5, 'Title must be 5 characters or more')
+            .max(30, 'Title must be 30 characters or less')
+            .required('Title is required'),
+        content: yup
+            .string()
+            .typeError('Content must be a string')
+            .min(30, 'Content must be 5 characters or more')
+            .required('Content is required'),
+        image: yup.string().typeError('Image must be a string').required('Image is required'),
+        categories: yup
+            .array()
+            .of(yup.string())
+            .min(1, 'At least one category is required')
+            .max(5, 'Categories must be 5 or less'),
+    })
 
     async function onSubmit(formValues: FormikValues, formHelpers: FormikHelpers<CreateFormValues>) {
         if (!user) return
@@ -59,23 +80,32 @@ export default function Create() {
         formRef?.current?.setFieldValue('image', cloudinaryData.url)
     }
 
+    const ValidationError = (message: string) => <p className="text-error">{message}</p>
+
     return (
         <PageWrapper>
-            <Formik initialValues={initialValues} onSubmit={onSubmit} innerRef={formRef}>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                innerRef={formRef}
+                validationSchema={validationSchema}>
                 {({ values, setFieldValue }) => {
-                    console.log(values)
                     return (
                         <Form>
                             <span className="form-control">
                                 <label className="label">Title</label>
+
                                 <Field name="title" type="text" className="input input-bordered" />
+                                <ErrorMessage name="title" component="p" className="text-error" />
                             </span>
 
                             <span className="form-control">
                                 <label className="label">Content</label>
+
                                 <Field name="content" type="text">
                                     {() => <textarea className="textarea textarea-bordered h-96"></textarea>}
                                 </Field>
+                                <ErrorMessage name="content" component="p" className="text-error" />
                             </span>
 
                             {values.image && (
@@ -86,15 +116,18 @@ export default function Create() {
 
                             <span className="form-control">
                                 <label className="label">Image</label>
+
                                 <input
                                     type="file"
                                     className="file-input file-input-bordered"
                                     onChange={({ target: { files } }) => uploadImage(files?.[0])}
                                 />
+                                <ErrorMessage name="image" component="p" className="text-error" />
                             </span>
 
                             <span className="form-control">
                                 <label className="label">Categories</label>
+
                                 <Select
                                     isMulti={true}
                                     onChange={(options: MultiValue<{ label: string; value: string }>) => {
@@ -119,6 +152,7 @@ export default function Create() {
                                         }),
                                     }}
                                 />
+                                <ErrorMessage name="categories" component="p" className="text-error" />
                             </span>
 
                             <div className="mt-10">
